3958053147f804638ea6cc92b1066c2e70188f2f
Sharla Gelfand <sharla.gelfand@gmail.com>
2020-02-05 16:01:26 -0500

    Re-render, functions to copy to blog too

--- render.R
+++ render.R
@@ -3,5 +3,9 @@
 # For online (with links in colour)
 rmarkdown::render("index.Rmd", output_file = "index.html", params = list(PDF_EXPORT = FALSE))
 
+# Update on blog :~)
+fs::file_copy("index.html", "/Users/sharla/github/sharlaparty/content/cv/index.html", overwrite = TRUE)
+usethis::proj_activate("/Users/sharla/github/sharlaparty/")
+
 # For PDF (with black italic links)
 rmarkdown::render("index.Rmd", output_file = "cv_for_pdf.html", params = list(PDF_EXPORT = TRUE))
